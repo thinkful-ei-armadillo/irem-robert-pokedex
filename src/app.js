20005cbd004c931ef607f56efed1eb1eb9e1176c
bobnearents <bobnearents@yahoo.com>
2019-02-21 13:28:36 -0800

    filter by search working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json')
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
@@ -14,8 +15,28 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req,res) => {
-  res.send('Hello, boilerplate !')
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychich`, `Rock`, `Steel`, `Water`];
+
+app.get('/pokemon', (req,res) => {
+  const {name, type} = req.query;
+  let results = POKEDEX.pokemon;
+  
+  if (name) {
+   results = results.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(name.toLowerCase())
+    })
+  }
+
+  if(type) {
+
+  }
+  //search name || type(one of valid types)
+  //users can request types -- get /types
+  res.send(results)
+})
+
+app.get('/types', (req,res) => {
+  res.send(validTypes);
 })
 
 app.use(function errorHandler(error, req, res, next) {
